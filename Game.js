77b07f59436fa079b47e3a2321ce8f713589ffd4
Davidcastro22 <107961097+Davidcastro22@users.noreply.github.com>
2022-07-15 14:26:40 -1200

    actualizacion 1.2

--- Game.js
+++ Game.js
@@ -11,12 +11,15 @@ let timerInical=30;
 let timer=30;
 let tiempoRegresivo=null;
 
+// sonidos
+
+
 // apuntando al documento html
 let mostrarMovimientos= document.getElementById("movimientos");
 let mostrarAciertos= document.getElementById("aciertos");
 let MostrarTiempo=document.getElementById("time");
-
-
+let fallaste=document.getElementById("fallaste");
+let jugarDeNuevo=document.getElementById("reintentar");
 // numeros aleatorios
 let numeros= [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
 // funccion "sort" ordena numeros de acuerdo a una funcion
@@ -27,25 +30,40 @@ console.log(numeros);
 
 // funciones
 
+
+
+
 // tiempo regresivo
 function contarTiempo(){
   tiempoRegresivo=  setInterval(() => {
         timer--;
-        MostrarTiempo.innerHTML=`Tiempo: ${timer}segundos`;
+        MostrarTiempo.innerHTML=`Tiempo: ${timer} segundos`;
+        if(timer<=10){
+            MostrarTiempo.style.color="red";
+           }
         // detener tiempo y detener juego
         if(timer==0){
             clearInterval(tiempoRegresivo);
             endGame();
+            MostrarTiempo.innerHTML=`Fallaste`;
+            MostrarTiempo.style.color="red";
+            MostrarTiempo.style.fontSize="20px"
+            fallaste.style.display="grid";
+            jugarDeNuevo.style.display="grid"
         }
     },1000);
 }
 function endGame(){
     for(let i=0; i<=15; i++){
         let finJuego=document.getElementById(i);
-        finJuego.innerHTML=numeros[i];
+        finJuego.innerHTML=`<img src="/estilos/img/${numeros[i]}.png" alt="">`;;
         finJuego.disabled=true;
     }
 }
+
+// iniciar juego otra vez
+
+
 
 
 
@@ -53,10 +71,12 @@ function endGame(){
 // al precionar un boton se activa esta funcion que tiene vinculado en el html
 function destapar(id){
 
+
    if(temporizador== false){
     contarTiempo();
     temporizador=true;
    }
+
    tarjetasDestapadas++;
 
    if(tarjetasDestapadas==1){
@@ -64,14 +84,14 @@ function destapar(id){
     tarjeta1=document.getElementById(id);
     // mostar valor de botones en el html
     primerResultado=numeros[id];
-    tarjeta1.innerHTML= primerResultado;
+    tarjeta1.innerHTML= `<img src="/estilos/img/${primerResultado}.png" alt="">`;
     // desabilitar el primer boton presionado
     tarjeta1.disabled=true;
    }else if(tarjetasDestapadas==2){
 // mostrar segunda tarjeta
 tarjeta2=document.getElementById(id);
 segundoResultado=numeros[id];
-tarjeta2.innerHTML= segundoResultado;
+tarjeta2.innerHTML= `<img src="/estilos/img/${segundoResultado}.png" alt="">`;
 //    desabilitar segundo boton
 tarjeta2.disabled=true;
 // aumentar movimientos
@@ -84,13 +104,23 @@ if(primerResultado==segundoResultado){
     tarjetasDestapadas=0;
     // aumentar aciertos
     aciertos++;
-    mostrarAciertos.innerHTML=`Aciertos${aciertos}`;
+    mostrarAciertos.innerHTML=`Aciertos: ${aciertos}`;
 
     if(aciertos==8){
         clearInterval(tiempoRegresivo);
-        mostrarAciertos.innerHTML=`Aciertos Completados`
-        MostrarTiempo.innerHTML= `Fantastico solo te domoraste ${timerInical-timer}segundos`
-        mostrarMovimientos.innerHTML= `Movimientos totales: ${movimientos}`
+        mostrarAciertos.innerHTML=`Aciertos Completados`;
+        mostrarAciertos.style=color="yellow";
+        MostrarTiempo.innerHTML= `Fantastico solo domoraste ${timerInical-timer}segundos`;
+        MostrarTiempo.style.color="yellow";
+        mostrarMovimientos.innerHTML= `Movimientos totales: ${movimientos}`;
+        mostrarMovimientos.style.color="yellow";
+        jugarDeNuevo.innerHTML='Jugar de nuevo';
+        jugarDeNuevo.style.display="grid";
+        fallaste.innerHTML="Felicidades";
+        fallaste.style.color="yellow";
+        fallaste.style.display="grid";
+        
+        
     }
 }
 
@@ -106,5 +136,7 @@ if(primerResultado==segundoResultado){
 }
    }
 }
+
+
 
 
